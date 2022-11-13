01eff9c312033cbcebeb4c007562594d8d21009b
Erick Luciano <102949948+Erickhbd@users.noreply.github.com>
2022-11-13 13:38:29 -0300

    funalização da aula #1 com banner

--- pages/index.js
+++ pages/index.js
@@ -40,39 +40,48 @@ export default HomePage
 //     )
 // }
 
+
   const StyledHeader = styled.div`
     img {
         width: 80px;
         height: 80px;
         border-radius: 50%;
     }
-
     .user-info {
-        margin-top: 50px;
         display: flex;
         align-items: center;
         width: 100%;
         padding: 16px 32px;
         gap: 16px;
     }
-  `;
-  
-  function Header () {
+`;
+
+const Banner = styled.div`
+    height:200px;
+    background-color: red;
+    background-image: url(${({bg})=> bg});
+     background-size: cover;
+     background-position: center;
+`;
+
+  function Header() {
     return (
         <StyledHeader>
-            {/* <img src="banner" /> */}
-
+            <Banner bg={config.banner}/>
             <section className="user-info">
-            <img src={`https://github.com/${config.github}.png`} />
-            <div>
-                <h2>{config.name}</h2>
-            </div>
-            <p>{config.job}</p>
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>
+                        {config.name}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
+                </div>
             </section>
-            
         </StyledHeader>
     )
-  }
+}
 
   function Timeline (propriedades) {
     console.log("dentro do componente", propriedades.playlists);
